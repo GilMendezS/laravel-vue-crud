304fa8f06d69ada364bd6bf567a053558f3100ce
Gilberto Méndez S <gilberto.mendez.santiz@gmail.com>
2018-12-11 16:11:54 -0600

    Some tips
    Greetings!

--- resources/assets/js/components/MiPensamientoComponent.vue
+++ resources/assets/js/components/MiPensamientoComponent.vue
@@ -25,21 +25,35 @@
         },
         methods: {
         	addPensamiento (pensamiento) {
-
-        		this.pensamientos.push(pensamiento)
+			this.pensamientos = [...this.pensamietos, pensamiento]// more elegant way! ;)
+        		//this.pensamientos.push(pensamiento)
         	},
         	eliminarElPensamiento(index) {
-        		this.pensamientos.splice(index,1)
+        		this.pensamientos.splice(index,1)// this is nice! but you can user es6
+			// sometimes you will have objects instead simple plain text, for example a thought with ids, in 
+			//thtat case you can use  aiflter method
+			//lets imagine that you have an object as below:
+			// const item = {  name: 'name', id: 1}
+			/*
+				removing the correct item without index variable
+				this.pensamientos.filter( p => p.id !== item.id )
+			
+			+/	
+			
         	},
         	updatePensamiento(index, pensamiento) {
         		this.pensamientos[index] = pensamiento
         	}
         },
         mounted () {
+	//always use a catch function to handle errors :)
             axios.get('/pensamientos')
             .then((response) => {
                 this.pensamientos = response.data
             })
+	    .catch(err => console.log(err)) // and even you can use a finally method to do somethig after to finish a http request
+	    .finally(() => console.log('do something'))
+	    
         }
     }
 </script>
